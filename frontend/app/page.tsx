6653b9bec14b8460711bf18bfcad9002a9f529fd
ShanilPraveen <jspraveen2002@gmail.com>
2025-07-01 10:51:23 +0530

    particles bg

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,17 +3,26 @@ import Tech from "../sections/Tech";
 import Projects from "../sections/Projects";
 import Blogs from "../sections/Blogs";
 import Contact from "../sections/Contact";
+import Particles from "../components/Particles";
 
 const HomePage = () => {
   return (
-    <>
-      <Hero/>
-      <Tech/>
-      <Projects/>
-      <Blogs/>
-      <Contact/>
-    </>
+    <div className="relative">
+      {/* <Particles
+        particleColors={["#3B82F6"]}
+        particleCount={100}
+        alphaParticles={true}
+        particleBaseSize={150}
+        sizeRandomness={1}
+        className="-z-10"
+      /> */}
 
+        <Hero />
+        <Tech />
+        <Projects />
+        <Blogs />
+        <Contact />
+    </div>
   );
 };
 
